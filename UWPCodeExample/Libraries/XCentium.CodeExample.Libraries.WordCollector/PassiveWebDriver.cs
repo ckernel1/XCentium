1d1594dba703c340b52a7553814e1681b606bd59
Russell Lambright <rlambright@alfains.com>
2018-08-19 02:02:27 -0400

    Added Wait screen and async processing.

---
+++ UWPCodeExample/Libraries/XCentium.CodeExample.Libraries.WordCollector/PassiveWebDriver.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XCentium.CodeExample.Libraries.WordCollector
+{
+    public class PassiveWebDriver<T> : IPassiveWebDriver<T> where T:class,IWebDriver
+    {
+        string _driverLocation = @".\Drivers";
+        public PassiveWebDriver(string driverLocation = null) => _driverLocation = string.IsNullOrWhiteSpace(driverLocation) ? _driverLocation : driverLocation;
+        public T GetWebDriver()
+        {
+            return Activator.CreateInstance(typeof(T), _driverLocation) as T;
+            
+        }
+
+        
+    }
+}
--- UWPCodeExample/XCentium.CodeExample.UI/Form1.cs
+++ UWPCodeExample/XCentium.CodeExample.UI/Form1.cs
@@ -19,62 +19,92 @@ using System.Collections;
 using System.IO;
 using System.Net;
 using System.Text.RegularExpressions;
+using OpenQA.Selenium;
 
 namespace XCentium.CodeExample.UI
 {
 
     public partial class Form1 : Form
     {
-        private BindingSource topWordsBingingSource = new BindingSource();
-
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        private BindingSource topWordsBindingSource = new BindingSource();
+        IBlacklist blacklist = Factory.CreateBlacklist(true);
+        IBlacklist customIgnorelist = CommonBlacklist.CreateFromTextFile(Properties.Resources.IgnoreList);
+        IWordStemmer stemmer = Factory.CreateWordStemmer(false);
+        IPassiveWebDriver<FirefoxDriver> webDriver = new PassiveWebDriver<FirefoxDriver>();
+        IProgressIndicator progressIndicator = new ProgressBarStatus();
+        delegate object ThreadedAPICall();
+        delegate void LocalizedUIOperation();
+        public Form1() => InitializeComponent();
+        
 
         private void btn_Go_Click(object sender, EventArgs e)
         {
-            IBlacklist blacklist = Factory.CreateBlacklist(true);
-            IBlacklist customIgnorelist = CommonBlacklist.CreateFromTextFile(Properties.Resources.IgnoreList);
-
-
-            using (var document = new UriExtractor(new ProgressBarStatus(), new FirefoxDriver(@".\Drivers")) { URI = new Uri(Normalize(txt_URL.Text)) })
+            Task task;
+            DoWait(() =>
             {
-                IWordStemmer stemmer = Factory.CreateWordStemmer(false);
-
-                var words = document
-                    .Filter(blacklist)
-                    .Filter(customIgnorelist)
-                    .CountOccurences()
-                    .GroupByStem(stemmer)
-                    .SortByOccurences();
-
-
-
-                var topWords = words.Take(10).ToList();
-                var sum = words.Sum(w => w.Occurrences);
-
-
-                var images = document.GetImages();
-                topWordsBingingSource.Clear();
-                topWords.ForEach(w => topWordsBingingSource.Add(w));
-                dgv_TopWords.DataSource = topWordsBingingSource;
-
-                ClearImageList();
-                foreach (var image in images)
+                task = Task.Run(() =>
                 {
+                    using (var document = new UriExtractor(progressIndicator, webDriver.GetWebDriver()) { URI = new Uri(Normalize(txt_URL.Text)) })
+                    {
+                        var words = document
+                            .Filter(blacklist)
+                            .Filter(customIgnorelist)
+                            .CountOccurences()
+                            .GroupByStem(stemmer)
+                            .SortByOccurences();
+
+                        // Compute top n words.
+                        var topWords = words.Take(10).ToList();
+                        var sum = words.Sum(w => w.Occurrences);
+
+                        DoLocal(() =>
+                        {
+                            // Clear previous list
+                            topWordsBindingSource.Clear();
+                            topWords.ForEach(w => topWordsBindingSource.Add(w));
+
+                            // Bind new words
+                            dgv_TopWords.DataSource = topWordsBindingSource;
+                            ClearImageList();
+                        });
 
 
-                    imagesFromCurrentSite.Images.Add(image.Item1, GetImage(image.Item1));
-                    lv_images.Items.Add(new ListViewItem(image.Item2, image.Item1));
 
-                }
+                        var images = document.GetImages();
+                        foreach (var image in images)
+                        {
+                            DoLocal(() => imagesFromCurrentSite.Images.Add(image.Item1, GetImage(image.Item1)));
+                            DoLocal(() => lv_images.Items.Add(new ListViewItem(image.Item2, image.Item1)));
+                        }
+                    }
+                });
+                task.GetAwaiter().OnCompleted(() =>
+                {
+                    if (task.Exception != null)
+                        MessageBox.Show($"Sorry the following error occured while trying to execute your last request:\r\n {GetErrorMessage(task.Exception)}");
+                });
+                task.Wait();
+            });
+            
 
-
-            }
+            // Set callback to check for any errors during execution
+            
+            
         }
 
+        /// <summary>
+        /// Recursive method to get the root cause of an exception
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        private string GetErrorMessage(Exception exception)
+        {
+            if (exception.InnerException == null)
+                return exception.Message;
+           return GetErrorMessage(exception.InnerException);
+        }
+
+       
         /// <summary>
         /// This removes images and disposes of them since we are done with them
         /// </summary>
@@ -84,6 +114,7 @@ namespace XCentium.CodeExample.UI
             foreach (Image image in imagesFromCurrentSite.Images)
                 image.Dispose();
             imagesFromCurrentSite.Images.Clear();
+            lv_images.Clear();
         }
 
         /// <summary>
@@ -122,6 +153,49 @@ namespace XCentium.CodeExample.UI
             {
                 new ViewerForm() {CurrentImage=GetImage(item.ImageKey),Title=item.Text }.Show(this);
             }
+            // Unselect indecies
+            (sender as ListView).SelectedIndices.Clear();
         }
+        /// <summary>
+        /// Handles tasks that need to be executed on the main thread.
+        /// </summary>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        private object DoLocal(Func<object> func)
+        {
+
+            return Invoke(new ThreadedAPICall(func));
+
+        }
+        /// <summary>
+        /// Handles tasks that need to be executed on the main thread.
+        /// </summary>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        private object DoLocal(Action func)
+        {
+
+            return Invoke(func);
+
+        }
+        private void DoWait<T>(Func<T> func) where T : class
+        {
+            var wait = new WaitForm();
+            func.BeginInvoke( new AsyncCallback(AysncAPICallEnded), wait);
+            wait.ShowDialog(this);
+        }
+        private void DoWait(Action func) 
+        {
+            var wait = new WaitForm();
+            func.BeginInvoke(new AsyncCallback(AysncAPICallEnded), wait);
+            wait.ShowDialog(this);
+        }
+        void AysncAPICallEnded(IAsyncResult result)
+        {
+            
+            if (result.IsCompleted)
+                Invoke(new LocalizedUIOperation((result.AsyncState as WaitForm).Close));
+        }
+
     }
 }
